bee73f032ee26dce938a9136784c061a4e47f053
Ivan1507 <52796964+Ivan1507@users.noreply.github.com>
2019-07-24 18:07:35 +0400

    Добавьте файлы проекта.

---
+++ Reflection/GeoAttribute.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reflection
+{
+    [AttributeUsage(AttributeTargets.Property|AttributeTargets.Constructor)]
+    class GeoAttribute:Attribute
+    {
+        public int X { get; set; }
+
+        public int Y { get; set; }
+
+        public GeoAttribute(){}
+        public GeoAttribute(int x,int y)
+        {
+            X = x;
+            Y = y;
+        }
+    }
+}
---
+++ Reflection/Photo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reflection
+{
+     
+    class Photo
+    {
+        [Geo(10, 20)]
+        public string Name { get; set; }
+         
+        public string Path { get; set; }
+       
+        public Photo(string name)
+        {
+            Name = name;
+        }
+    }
+}
